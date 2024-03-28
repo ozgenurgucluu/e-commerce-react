43a26edbb2dd22ca65c273ca716f07735047d13e
Özgenur Güçlü <ozgenurgucluu@gmail.com>
2024-03-28 22:26:49 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+      <RouterProvider router={router} />,
+
+)
